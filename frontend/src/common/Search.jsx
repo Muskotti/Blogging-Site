370ab0e3c163c6820205fd7efea0b1fb87d1a8a9
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-14 20:07:20 +0200

    Search textfield
    Add: Search textfield

---
+++ frontend/src/common/Search.jsx
@@ -0,0 +1,19 @@
+import {TextField, FontIcon} from "react-md";
+import React, {PureComponent} from "react";
+
+export default class Search extends PureComponent {
+    render() {
+        return (
+            <div  className="md-grid">
+                <TextField
+                    id="floating-center-title"
+                    lineDirection="center"
+                    placeholder={"Search"}
+                    resize={{min: 640}}
+                    leftIcon={<FontIcon>search</FontIcon>}
+                    className="md-cell md-cell--bottom "
+                />
+            </div>
+        );
+    }
+}
