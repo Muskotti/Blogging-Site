5148061794fd49354851ab178ba1aa691d62f60b
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-14 15:11:33 +0200

    Logging dialog
    Add: Logging dialog

---
+++ frontend/src/common/LoggingDialogs.jsx
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react';
+import { Button, DialogContainer, TextField } from 'react-md';
+
+export default class LoggingDialogs extends PureComponent {
+  state = { visible: false };
+
+  show = () => {
+    this.setState({ visible: true });
+  };
+
+  hide = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    const { visible } = this.state;
+
+    const actions = [];
+    actions.push(<Button flat secondary swapTheming onClick={this.hide}>Cancel</Button>);
+    actions.push(<Button flat primary swapTheming onClick={this.hide}>Confirm</Button>);
+
+    return (
+      <div>
+        <Button flat secondary swapTheming onClick={this.show}>Logging</Button>
+        <DialogContainer
+            id="simple-action-dialog"
+            visible={visible}
+            onHide={this.hide}
+            actions={actions}
+            title="Logging"
+        >
+          <TextField
+              id="Username"
+              placeholder="Username"
+          />
+          <TextField
+              id="password"
+              label="Enter your password"
+              type="password"
+          />
+        </DialogContainer>
+      </div>
+    );
+  }
+}
