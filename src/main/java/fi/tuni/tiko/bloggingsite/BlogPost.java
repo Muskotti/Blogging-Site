46996100376a468c0d7af9a427504883f444777c
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-14 14:04:33 +0200

    Change BlogPost date to timestamp
    - Change field date of BlogPost from java.sql.Date to java.sql.Timestamp
    - Test persistence to database when running application

--- src/main/java/fi/tuni/tiko/bloggingsite/BlogPost.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/BlogPost.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
 public class BlogPost {
@@ -15,5 +16,11 @@ public class BlogPost {
 
     private String author;
 
-    private Date date;
+    private Timestamp date;
+
+    public BlogPost(String postText, String author, Timestamp date) {
+        this.postText = postText;
+        this.author = author;
+        this.date = date;
+    }
 }
--- src/main/java/fi/tuni/tiko/bloggingsite/BloggingsiteApplication.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/BloggingsiteApplication.java
@@ -1,13 +1,25 @@
 package fi.tuni.tiko.bloggingsite;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 @SpringBootApplication
-public class BloggingsiteApplication {
+public class BloggingsiteApplication implements CommandLineRunner {
+
+	@Autowired
+	BlogPostRepository postRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingsiteApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		postRepository.save(new BlogPost("Test text", "Test Author", new Timestamp(System.currentTimeMillis())));
+	}
 }
