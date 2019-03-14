3591b1bc9b15b24791bf37d4328ab196bd208a15
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-14 17:52:34 +0200

    Feature: Get BlogPost by ID

--- src/main/java/fi/tuni/tiko/bloggingsite/Controller.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/Controller.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -26,5 +28,17 @@ public class Controller {
     @GetMapping("/posts")
     public Iterable<BlogPost> findAllBlogPosts() {
         return postRepository.findAll();
+    }
+
+    //TODO Create exception if post can't be found by provided id.
+    @GetMapping("/posts/{id}")
+    public BlogPost findPostById(@RequestParam Long id) {
+        Optional<BlogPost> post = postRepository.findById(id);
+
+        if (post.isPresent()) {
+            return post.get();
+        }
+
+        return null;
     }
 }
