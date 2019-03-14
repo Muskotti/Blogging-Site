91679980df3613b9e425146b99b7296bfa16c118
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-14 20:08:14 +0200

    New blog post button
    Add: Button to add new blog posts

---
+++ frontend/src/common/NewBlogPost.jsx
@@ -0,0 +1,10 @@
+import {Button} from "react-md";
+import React, {PureComponent} from "react";
+
+export default class NewBlogPost extends PureComponent {
+    render() {
+        return (
+            <Button floating secondary>add</Button>
+        );
+    }
+}
