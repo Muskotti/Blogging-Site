f71c36481fe4305cf946c11cffd61e0feb2219ac
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-14 14:16:20 +0200

    Implement REST retrieving of all BlogPosts

--- src/main/java/fi/tuni/tiko/bloggingsite/BlogPost.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/BlogPost.java
@@ -17,9 +17,36 @@ public class BlogPost {
 
     private Timestamp time;
 
+    public BlogPost() {
+    }
+
     public BlogPost(String postText, String author, Timestamp time) {
         this.postText = postText;
         this.author = author;
+        this.time = time;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
         this.time = time;
     }
 }
--- src/main/java/fi/tuni/tiko/bloggingsite/Controller.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/Controller.java
@@ -1,14 +1,30 @@
 package fi.tuni.tiko.bloggingsite;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
 public class Controller {
+    @Autowired
+    BlogPostRepository postRepository;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello, the time at the server is now " + new Date() + "\n";
+    }
+
+    @PostMapping("/post")
+    public BlogPost saveBlogPost(@RequestBody BlogPost post) {
+        return postRepository.save(post);
+    }
+
+    @GetMapping("/posts")
+    public Iterable<BlogPost> findAllBlogPosts() {
+        return postRepository.findAll();
     }
 }
