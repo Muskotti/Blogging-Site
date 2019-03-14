60380027df6de04b26faa448156beda309260717
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-14 15:12:46 +0200

    Change method of finding comments by post
    - Alter CommentRepository, so that you can find all comments by post id,
      and not a BlogPost object

--- src/main/java/fi/tuni/tiko/bloggingsite/CommentRepository.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/CommentRepository.java
@@ -3,5 +3,5 @@ package fi.tuni.tiko.bloggingsite;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-    Iterable<Comment> findAllByPost(BlogPost post);
+    Iterable<Comment> findAllByPost_Id(Long post_id);
 }
