8185523fe9109c6efa0af58d601a6127c5dda780
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-15 14:34:03 +0200

    Add partial HATEOAS implementation

--- src/main/java/fi/tuni/tiko/bloggingsite/CommentRepository.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/CommentRepository.java
@@ -2,6 +2,10 @@ package fi.tuni.tiko.bloggingsite;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends CrudRepository<Comment, Long> {
     Iterable<Comment> findAllByPostId(Long post_id);
+
+    Optional<Comment> findByIdAndAndPostId(Long id, Long post_id);
 }
