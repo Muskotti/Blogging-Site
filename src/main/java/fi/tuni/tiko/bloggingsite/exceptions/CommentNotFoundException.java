937fb7553780fb0cf1befd2a9069c344a3e8e09b
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-15 16:17:28 +0200

    Add Http status to CommentNotFoundException

--- src/main/java/fi/tuni/tiko/bloggingsite/exceptions/CommentNotFoundException.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/exceptions/CommentNotFoundException.java
@@ -1,5 +1,9 @@
 package fi.tuni.tiko.bloggingsite.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CommentNotFoundException extends RuntimeException {
     public CommentNotFoundException(long id) {
         super("Comment not found by id:" + id);
