e26957c979b9c35c80216c41ac177729e40ac421
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-15 19:16:43 +0200

    New Cards and refresh on new blog post
    Add: cards margin and proper titles and stuff
    Add: reload to new blog post

--- frontend/src/common/CardsTest.jsx
+++ frontend/src/common/CardsTest.jsx
@@ -3,20 +3,18 @@ import React, {PureComponent} from "react";
 
 export default class CardsTest extends PureComponent {
     render() {
-        const style = { minWidth: 500, maxWidth: 640};
+        const style = { minWidth: 500, maxWidth: 640, marginBottom: 20};
         return (
-            <div>
                 <Card style={style} className="md-block-centered">
-                    <CardTitle title={"Title"} subtitle={"By: MIKA"}/>
+                    <CardTitle title={this.props.title} subtitle={this.props.author}/>
                     <CardText style={{textAlign: 'left'}}>
-                        <p>LOLOLOOLOLOLLOL</p>
-                        <CardActions>
+                        <p>{this.props.content}</p>
+                        <CardActions expander>
                             <Button icon secondary swapTheming>favorite</Button>
                             <Button flat>Comment</Button>
                         </CardActions>
                     </CardText>
                 </Card>
-            </div>
         )
     }
 }
