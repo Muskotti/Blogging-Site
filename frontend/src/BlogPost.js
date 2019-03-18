cf216675e2c9aa8ac586015210ca7553de9790e7
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-18 15:44:53 +0200

    Add unknown small changes to Frontend files

---
+++ frontend/src/BlogPost.js
@@ -0,0 +1,13 @@
+export default class BlogPost {
+    constructor(responseJson) {
+
+    }
+
+    constructor(author, title, content) {
+
+    }
+
+    set author(author) {
+        this.author = author;
+    }
+}
