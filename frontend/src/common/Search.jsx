3b45867bdd5a1993db56e62e126b80017d2c4a13
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-28 15:26:20 +0200

    Search function and moved data fetch
    Add: search function
    Moved: fetch posts from BlogPosts.jsx to App.js

--- frontend/src/common/Search.jsx
+++ frontend/src/common/Search.jsx
@@ -1,17 +1,25 @@
-import {TextField, FontIcon} from "react-md";
+import {Autocomplete, FontIcon} from "react-md";
 import React, {PureComponent} from "react";
 
 export default class Search extends PureComponent {
     render() {
+        let titles = this.props.data.map(item => (
+            item.title
+        ))
+
         return (
             <div  className="md-grid">
-                <TextField
+                <Autocomplete
                     id="floating-center-title"
                     lineDirection="center"
                     placeholder={"Search"}
                     resize={{min: 640, max: 1000}}
                     leftIcon={<FontIcon>search</FontIcon>}
                     className="md-cell md-cell--bottom "
+                    data={titles}
+                    filter={Autocomplete.caseInsensitiveFilter}
+                    onAutocomplete={this.props.onAutocomplete}
+                    onChange={this.props.onChange}
                 />
             </div>
         );
