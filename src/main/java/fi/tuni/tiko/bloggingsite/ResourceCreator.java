493cda872302dc7880023b283e283acc89e15b14
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-03 12:32:48 +0300

    Update blogpost resource creation

--- src/main/java/fi/tuni/tiko/bloggingsite/ResourceCreator.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/ResourceCreator.java
@@ -4,8 +4,12 @@ import fi.tuni.tiko.bloggingsite.blogpost.BlogPost;
 import fi.tuni.tiko.bloggingsite.blogpost.BlogPostController;
 import fi.tuni.tiko.bloggingsite.comment.Comment;
 import fi.tuni.tiko.bloggingsite.comment.CommentController;
+import fi.tuni.tiko.bloggingsite.exceptions.UnauthorizedException;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -21,22 +25,84 @@ public class ResourceCreator {
                 .withSelfRel()
                 .withType(GET);
 
+        Link like = linkTo(methodOn(BlogPostController.class).likeBlogPostById(post.getId()))
+                .withRel("like")
+                .withType(PUT);
+
+
+        Link dislike = linkTo(methodOn(BlogPostController.class).dislikeBlogPostById(post.getId()))
+                .withRel("dislike")
+                .withType(PUT);
+
+        Optional<Link> edit;
+        try {
+            edit = Optional.of(linkTo(methodOn(BlogPostController.class).editBlogPostById(post))
+                    .withRel("edit")
+                    .withType(PUT));
+        } catch (UnauthorizedException e) {
+            edit = Optional.empty();
+        }
+
+        Optional<Link> delete;
+        try {
+            delete = Optional.of(linkTo(methodOn(BlogPostController.class).deleteBlogPostById(post.getId()))
+                    .withRel("edit")
+                    .withType(DEL));
+        } catch (UnauthorizedException e) {
+            delete = Optional.empty();
+        }
+
         Link comments = linkTo(methodOn(CommentController.class).findCommentsByPostId(post.getId()))
                 .withRel("comments")
                 .withType(GET);
 
-        Link like = linkTo(methodOn(BlogPostController.class).likeBlogPostById(post.getId()))
-                .withRel("like")
-                .withType(PUT);
-        Link dislike = linkTo(methodOn(BlogPostController.class).dislikeBlogPostById(post.getId())).withRel("dislike");
-        return new Resource<>(post, selfRel, comments, like, dislike);
+        Link addComment = linkTo(methodOn(CommentController.class).saveCommentToBlogPostByPostId(
+                post.getId(), new Comment("Comment content here", post)))
+                .withRel("add comment")
+                .withType(POST);
+
+        Link[] links = createLinkArray(optionalLinkArray(edit, delete),
+                selfRel, like, dislike, comments, addComment);
+
+        return new Resource<>(post, links);
     }
 
     public static Resource<Comment> createCommentResource(Comment comment) {
         Link selfRel = linkTo(methodOn(CommentController.class).
-                findCommentsByIdAndByPostId(comment.getId(), comment.getPost().getId())).withSelfRel();
+                findCommentsByIdAndByPostId(comment.getId(), comment.getPost().getId()))
+                .withSelfRel()
+                .withType(GET);
+
         Link relatedPost = linkTo(methodOn(BlogPostController.class).
-                findBlogPostById(comment.getPost().getId())).withRel("relatedPost");
+                findBlogPostById(comment.getPost().getId()))
+                .withRel("relatedPost")
+                .withType(GET);
+
+
         return new Resource<>(comment, selfRel, relatedPost);
+    }
+
+    private static Link[] createLinkArray(Link[] optionalLinks, Link... commonLinks) {
+        Link[] combinedLinks = new Link[optionalLinks.length + commonLinks.length];
+
+        for (int i = 0; i < commonLinks.length; i++) {
+            combinedLinks[i] = commonLinks[i];
+        }
+
+        for (int i = 0; i < optionalLinks.length; i++) {
+            combinedLinks[commonLinks.length - 1 + i] = optionalLinks[i];
+        }
+
+        return combinedLinks;
+    }
+
+    private static Link[] optionalLinkArray(Optional<Link>... optionalLinks) {
+        ArrayList<Link> links = new ArrayList<>(optionalLinks.length);
+
+        for (Optional<Link> optionalLink : optionalLinks) {
+            optionalLink.ifPresent(link -> links.add(link));
+        }
+
+        return links.toArray(new Link[0]);
     }
 }
--- src/main/java/fi/tuni/tiko/bloggingsite/blogpost/BlogPostController.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/blogpost/BlogPostController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -107,10 +109,11 @@ public class BlogPostController {
     }
 
     @DeleteMapping("/{id}/delete")
-    public void deleteBlogPostById(@PathVariable Long id) throws BlogPostIdNotFoundException, UnauthorizedException {
+    public ResponseEntity<Void> deleteBlogPostById(@PathVariable Long id) throws BlogPostIdNotFoundException, UnauthorizedException {
         if (loginController.userIsAdmin()) {
             BlogPost post = findBlogPostById(id).getContent();
             postRepository.delete(post);
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
             throw new UnauthorizedException();
         }
