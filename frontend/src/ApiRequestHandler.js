71b14712d18c3b8d9856030b13ed81d00d6be301
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-03 17:11:37 +0300

    Update ApiRequestHandler
    - Rename function getCreateBlogPostRequest to createBlogPostRequest
    - Use array.find() instead of own function with iterative search
    - Define fetch mode as no-cors

--- frontend/src/ApiRequestHandler.js
+++ frontend/src/ApiRequestHandler.js
@@ -3,32 +3,21 @@ import ApiRequest from "./ApiRequest";
 export default class ApiRequestHandler {
 
   //Returns an ApiRequest object for requesting all blog posts
-  static async getAllBlogPostsRequest() {
-    return this.getRequestByRel('getPosts');
-  }
-
-  static async getCreateBlogPostRequest() {
-    return this.getRequestByRel('createPost');
-  }
-
-  static async getRequestByRel(rel) {
-    let apiRoot = '/api/';
-
-    const httpResp = await fetch(apiRoot);
-    const resourceJson = await httpResp.json();
-    const link = this.getLinkFromArrayByRel(resourceJson.links, rel);
-    return new ApiRequest(link.href, link.type);
-  }
-
-  static getLinkFromArrayByRel(links, rel) {
-    let link = null;
-
-    for (let _link of links) {
-      if (_link.rel === rel) {
-        link = _link;
-      }
+    static async getAllBlogPostsRequest() {
+        return this.getRequestByRel('getPosts');
     }
 
-    return link;
-  }
+    static async createBlogPostRequest() {
+        return this.getRequestByRel('createPost');
+    }
+
+    static async getRequestByRel(rel) {
+        let apiRoot = './api/';
+
+        const httpResp = await fetch(apiRoot, {mode: "no-cors"});
+        const resourceJson = await httpResp.json();
+        const link = resourceJson.links.find((linkObject) => linkObject.rel === rel);
+
+        return new ApiRequest(link.href, link.type);
+    }
 }
