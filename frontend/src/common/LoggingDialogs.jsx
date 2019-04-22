4c58fecb863340abe6c39412b181b999787fea76
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-04-22 20:00:59 +0300

    login check
    Add: logIn function where login status is checked

--- frontend/src/common/LoggingDialogs.jsx
+++ frontend/src/common/LoggingDialogs.jsx
@@ -2,7 +2,17 @@ import React, { PureComponent } from 'react';
 import { Button, DialogContainer, TextField } from 'react-md';
 
 export default class LoggingDialogs extends PureComponent {
-  state = { visible: false };
+
+  constructor(props) {
+    super(props);
+
+    this.username = React.createRef();
+    this.password = React.createRef();
+  }
+
+  state = {
+    visible: false
+  };
 
   show = () => {
     this.setState({ visible: true });
@@ -12,12 +22,26 @@ export default class LoggingDialogs extends PureComponent {
     this.setState({ visible: false });
   };
 
+  logIn = () => {
+      this.hide();
+      let result = true;
+      /* TODO: check loggin from backend
+      let obj = {
+        "title": this.titleField.current.value,
+        "author": this.authorField.current.value,
+        "content": this.contentField.current.value,
+        "time": new Date().getTime()
+      }
+      */
+       this.props.setLoginStatus(result)
+  }
+
   render() {
     const { visible } = this.state;
 
     const actions = [];
     actions.push(<Button flat secondary swapTheming onClick={this.hide}>Cancel</Button>);
-    actions.push(<Button flat primary swapTheming onClick={this.hide}>Confirm</Button>);
+    actions.push(<Button flat primary swapTheming onClick={this.logIn}>Confirm</Button>);
 
     return (
       <div>
@@ -32,11 +56,13 @@ export default class LoggingDialogs extends PureComponent {
           <TextField
               id="Username"
               placeholder="Username"
+              ref={this.username}
           />
           <TextField
               id="password"
               label="Enter your password"
               type="password"
+              ref={this.password}
           />
         </DialogContainer>
       </div>
