698b0249b5c69aac5186b9146a97aea832b147cb
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-24 19:36:42 +0300

    Update frontend resources to backend

---
+++ src/main/resources/static/precache-manifest.05ed72daf4de546d6adfa4ac4e816b6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e6711f0ee22c18562a0",
+    "url": "/static/css/main.4800657b.chunk.css"
+  },
+  {
+    "revision": "8e6711f0ee22c18562a0",
+    "url": "/static/js/main.c6625f70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "72fa87eb25a89df02356",
+    "url": "/static/js/2.85420102.chunk.js"
+  },
+  {
+    "revision": "b000caad592321fddd92b96bcc988001",
+    "url": "/index.html"
+  }
+];
