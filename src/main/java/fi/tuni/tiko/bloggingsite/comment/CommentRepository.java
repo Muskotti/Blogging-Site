e00aef4a247fa58bba53f8191c305f20ec36e598
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-24 10:58:10 +0300

    Add javadoc for comment package

--- src/main/java/fi/tuni/tiko/bloggingsite/comment/CommentRepository.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/comment/CommentRepository.java
@@ -4,8 +4,25 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+/**
+ * CRUD repository for Comments.
+ *
+ * @author Anton Höglund
+ */
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+    /**
+     * Finds Iterable of Comments by blog post id.
+     * @param post_id the id of the blog post.
+     * @return the Iterable of Comments.
+     */
     Iterable<Comment> findAllByPostId(Long post_id);
 
+    /**
+     * Finds a single comment by it's id and the id of the blog post it's related to.
+     * @param id the id of the comment.
+     * @param post_id the id of the blog post.
+     * @return the single comment in Optional wrapper.
+     */
     Optional<Comment> findByIdAndAndPostId(Long id, Long post_id);
 }
