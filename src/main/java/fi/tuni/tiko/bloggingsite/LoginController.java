ec08ca7ae253b014356c620b625573b710f63286
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-24 10:43:45 +0300

    Update javadoc for classes in exceptions package

--- src/main/java/fi/tuni/tiko/bloggingsite/LoginController.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/LoginController.java
@@ -3,6 +3,9 @@ package fi.tuni.tiko.bloggingsite;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * TODO javadoc
+ */
 @RestController
 public class LoginController {
     private UserType userType = UserType.ADMIN;
--- src/main/java/fi/tuni/tiko/bloggingsite/exceptions/BlogPostIdNotFoundException.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/exceptions/BlogPostIdNotFoundException.java
@@ -3,25 +3,22 @@ package fi.tuni.tiko.bloggingsite.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when a client accesses a blog post that cannot be found.
+ *
+ * Exception thrown when a client accesses a comment that cannot be found. Responds to the client with
+ * HTTP status 404 (Not found) when thrown.
+ *
+ * @author Anton Höglund
+ */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BlogPostIdNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs the Exception with a default message referring to the id of the blog post not found.
+     * @param id the id of the blog post.
+     */
     public BlogPostIdNotFoundException(long id) {
         super("A BlogPost could not be found from the repository with id:" + id);
-    }
-
-    public BlogPostIdNotFoundException() {
-        super();
-    }
-
-    public BlogPostIdNotFoundException(String message) {
-        super(message);
-    }
-
-    public BlogPostIdNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public BlogPostIdNotFoundException(Throwable cause) {
-        super(cause);
     }
 }
