1fe390d0476132e05d043d2de59dc48c68d5bbdf
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-24 21:41:36 +0300

    Add javadoc to LoginController

--- src/main/java/fi/tuni/tiko/bloggingsite/LoginController.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/LoginController.java
@@ -7,18 +7,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * TODO javadoc
+ * Handles requests to login to the application.
+ *
+ * Handles requests to login to the application. Implementation is just a dummy, it always recognizes
+ * the requestor as UserType ADMIN. This class also provides a way for other classes to check the UserType of
+ * the client.
+ *
+ * @author Anton Höglund
  */
 @RestController
 public class LoginController {
+
+    /**
+     * The UserType of the client.
+     */
     private UserType userType = UserType.ADMIN;
 
+    /**
+     * Handles requests to login.
+     * @return a ResponseEntity with a HttpStatus.
+     */
     @PostMapping("/login")
     public ResponseEntity<Void> login() {
         userType = UserType.ADMIN;
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * Returns true if the client is an admin, false if not.
+     * @return true if the client is an admin, false if not.
+     */
     public boolean userIsAdmin() {
         return userType == UserType.ADMIN;
     }
