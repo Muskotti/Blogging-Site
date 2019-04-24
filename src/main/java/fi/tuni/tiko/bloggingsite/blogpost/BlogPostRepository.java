914bf5990486f263ccec82483a5ba8b177d4c179
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-24 12:06:40 +0300

    Fix typo in javadoc

--- src/main/java/fi/tuni/tiko/bloggingsite/blogpost/BlogPostRepository.java
+++ src/main/java/fi/tuni/tiko/bloggingsite/blogpost/BlogPostRepository.java
@@ -3,7 +3,7 @@ package fi.tuni.tiko.bloggingsite.blogpost;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Simple CRUD repostitory for blog posts.
+ * Simple CRUD repository for blog posts.
  */
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
 }
