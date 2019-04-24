7c0c7550c294b643551bf68fd1b37050fd625f80
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-24 14:00:05 +0300

    Try fixing build failure

---
+++ src/main/resources/static/precache-manifest.73e9cc5af24e94496e1a3ea7470a175d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ea4b3a5d54de1375f95",
+    "url": "/static/css/main.4800657b.chunk.css"
+  },
+  {
+    "revision": "3ea4b3a5d54de1375f95",
+    "url": "/static/js/main.a3c7014e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "72fa87eb25a89df02356",
+    "url": "/static/js/2.85420102.chunk.js"
+  },
+  {
+    "revision": "148cea6534f400c6a337a19bd80a03a0",
+    "url": "/index.html"
+  }
+];
