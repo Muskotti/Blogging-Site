82cafa14eba386513c6f1d1f6e9bf18c41650c74
Anton Höglund <antonjhoglund@hotmail.com>
2019-05-06 11:45:51 +0300

    Remove source code, rename jar

--- frontend/src/ApiRequest.js
+++
@@ -1,6 +0,0 @@
-export default class ApiRequest {
-  constructor(url, requestType) {
-    this.url = url;
-    this.requestType = requestType;
-  }
-}
--- src/main/java/fi/tuni/tiko/bloggingsite/blogpost/BlogPostRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.tuni.tiko.bloggingsite.blogpost;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Simple CRUD repository for blog posts.
- */
-public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-}
